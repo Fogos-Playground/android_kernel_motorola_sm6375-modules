ArianK16a/android_kernel_fairphone_sm7635-modules

qcom: Fix paths for compiling for inline
Change-Id: I962ea4d98f1b3a46133223a9f70ada50faeeb032
Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

qcom/opensource/dataipa/drivers/platform/msm/gsi/gsi_trace.h
@@ -49,7 +49,7 @@ TRACE_EVENT(
 /* This part must be outside protection */
 #ifndef GSI_TRACE_INCLUDE_PATH
 #ifdef CONFIG_IPA_VENDOR_DLKM
-#define GSI_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/dataipa/drivers/platform/msm/gsi
+#define GSI_TRACE_INCLUDE_PATH ../../../../sm8650-modules/qcom/opensource/dataipa/drivers/platform/msm/gsi
 #else
 #define GSI_TRACE_INCLUDE_PATH ../../techpack/dataipa/drivers/platform/msm/gsi
 #endif

qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_clients/rndis_ipa_trace.h
@@ -72,7 +72,7 @@ TRACE_EVENT(
 /* This part must be outside protection */
 #ifndef RNDIS_TRACE_INCLUDE_PATH
 #ifdef CONFIG_IPA_VENDOR_DLKM
-#define RNDIS_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_clients
+#define RNDIS_TRACE_INCLUDE_PATH ../../../../sm8650-modules/qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_clients
 #else
 #define RNDIS_TRACE_INCLUDE_PATH ../../techpack/dataipa/drivers/platform/msm/ipa/ipa_clients
 #endif

qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_v3/ipa_trace.h
@@ -394,7 +394,7 @@ TRACE_EVENT(
 /* This part must be outside protection */
 #ifndef IPA_TRACE_INCLUDE_PATH
 #ifdef CONFIG_IPA_VENDOR_DLKM
-#define IPA_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_v3
+#define IPA_TRACE_INCLUDE_PATH ../../../../sm8650-modules/qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_v3
 #else
 #define IPA_TRACE_INCLUDE_PATH ../../techpack/dataipa/drivers/platform/msm/ipa/ipa_v3
 #endif

qcom/opensource/datarmnet/core/dfc.h
@@ -18,14 +18,14 @@
 #ifndef RMNET_TRACE_INCLUDE_PATH
 #if defined(CONFIG_RMNET_LA_PLATFORM)
 #ifdef CONFIG_ARCH_KHAJE
-#define RMNET_TRACE_INCLUDE_PATH ../../../../../vendor/qcom/opensource/datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../../sm8650-modules/qcom/opensource/datarmnet/core
 #else
-#define RMNET_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../sm8650-modules/qcom/opensource/datarmnet/core
 #endif /* CONFIG_ARCH_KHAJE */
 #elif defined(__arch_um__)
 #define RMNET_TRACE_INCLUDE_PATH ../../datarmnet/core
 #else
-#define RMNET_TRACE_INCLUDE_PATH ../../../../../../../datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../sm8650-modules/qcom/opensource/datarmnet/core
 #endif /* defined(CONFIG_RMNET_LA_PLATFORM) */
 #endif /* RMNET_TRACE_INCLUDE_PATH */
 

qcom/opensource/datarmnet/core/rmnet_trace.h
@@ -9,11 +9,11 @@
 
 #ifndef RMNET_TRACE_INCLUDE_PATH
 #if defined(CONFIG_RMNET_LA_PLATFORM)
-#define RMNET_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../sm8650-modules/qcom/opensource/datarmnet/core
 #elif defined(__arch_um__)
 #define RMNET_TRACE_INCLUDE_PATH ../../datarmnet/core
 #else
-#define RMNET_TRACE_INCLUDE_PATH ../../../../../../../datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../sm8650-modules/qcom/opensource/datarmnet/core
 #endif /* defined(CONFIG_RMNET_LA_PLATFORM) */
 #endif /* RMNET_TRACE_INCLUDE_PATH */
 #define TRACE_INCLUDE_PATH RMNET_TRACE_INCLUDE_PATH

qcom/opensource/datarmnet/core/wda.h
@@ -10,14 +10,14 @@
 #ifndef RMNET_TRACE_INCLUDE_PATH
 #if defined(CONFIG_RMNET_LA_PLATFORM)
 #ifdef CONFIG_ARCH_KHAJE
-#define RMNET_TRACE_INCLUDE_PATH ../../../../../vendor/qcom/opensource/datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../../sm8650-modules/qcom/opensource/datarmnet/core
 #else
-#define RMNET_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../sm8650-modules/qcom/opensource/datarmnet/core
 #endif /* CONFIG_ARCH_KHAJE */
 #elif defined(__arch_um__)
 #define RMNET_TRACE_INCLUDE_PATH ../../datarmnet/core
 #else
-#define RMNET_TRACE_INCLUDE_PATH ../../../../../../../datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../sm8650-modules/qcom/opensource/datarmnet/core
 #endif /* defined(CONFIG_RMNET_LA_PLATFORM) */
 #endif /* RMNET_TRACE_INCLUDE_PATH */
 #define TRACE_INCLUDE_PATH RMNET_TRACE_INCLUDE_PATH

qcom/opensource/dsp-kernel/dsp/fastrpc_trace.h
@@ -15,7 +15,7 @@
  * Define path if not defined in bazel file
  */
 #ifndef DSP_TRACE_INCLUDE_PATH
-#define DSP_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/dsp-kernel/dsp
+#define DSP_TRACE_INCLUDE_PATH ../../../../sm8650-modules/qcom/opensource/dsp-kernel/dsp
 #endif
 
 #undef TRACE_INCLUDE_PATH

qcom/opensource/securemsm-kernel/smcinvoke/trace_smcinvoke.h
@@ -490,7 +490,7 @@ TRACE_EVENT(smcinvoke_release,
 * Define path if not defined in bazel file
  */
 #ifndef SMCINVOKE_TRACE_INCLUDE_PATH
-#define SMCINVOKE_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/securemsm-kernel/smcinvoke
+#define SMCINVOKE_TRACE_INCLUDE_PATH ../../../../sm8650-modules/qcom/opensource/securemsm-kernel/smcinvoke
 #endif
 
 #undef TRACE_INCLUDE_PATH

Fogos-Playground/android_kernel_motorola_sm6375-modules

qcom/opensource/dataipa/drivers/platform/msm/gsi/gsi_trace.h
@@ -49,7 +49,7 @@ TRACE_EVENT(
 /* This part must be outside protection */
 #ifndef GSI_TRACE_INCLUDE_PATH
 #ifdef CONFIG_IPA_VENDOR_DLKM
-#define GSI_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/dataipa/drivers/platform/msm/gsi
+#define GSI_TRACE_INCLUDE_PATH ../../../../sm6375-modules/qcom/opensource/dataipa/drivers/platform/msm/gsi
 #else
 #define GSI_TRACE_INCLUDE_PATH ../../techpack/dataipa/drivers/platform/msm/gsi
 #endif

qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_clients/rndis_ipa_trace.h
@@ -72,7 +72,7 @@ TRACE_EVENT(
 /* This part must be outside protection */
 #ifndef RNDIS_TRACE_INCLUDE_PATH
 #ifdef CONFIG_IPA_VENDOR_DLKM
-#define RNDIS_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_clients
+#define RNDIS_TRACE_INCLUDE_PATH ../../../../sm6375-modules/qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_clients
 #else
 #define RNDIS_TRACE_INCLUDE_PATH ../../techpack/dataipa/drivers/platform/msm/ipa/ipa_clients
 #endif

qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_v3/ipa_trace.h
@@ -394,7 +394,7 @@ TRACE_EVENT(
 /* This part must be outside protection */
 #ifndef IPA_TRACE_INCLUDE_PATH
 #ifdef CONFIG_IPA_VENDOR_DLKM
-#define IPA_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_v3
+#define IPA_TRACE_INCLUDE_PATH ../../../../sm6375-modules/qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_v3
 #else
 #define IPA_TRACE_INCLUDE_PATH ../../techpack/dataipa/drivers/platform/msm/ipa/ipa_v3
 #endif

qcom/opensource/datarmnet/core/dfc.h
@@ -18,14 +18,14 @@
 #ifndef RMNET_TRACE_INCLUDE_PATH
 #if defined(CONFIG_RMNET_LA_PLATFORM)
 #ifdef CONFIG_ARCH_KHAJE
-#define RMNET_TRACE_INCLUDE_PATH ../../../../../vendor/qcom/opensource/datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../../sm6375-modules/qcom/opensource/datarmnet/core
 #else
-#define RMNET_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../sm6375-modules/qcom/opensource/datarmnet/core
 #endif /* CONFIG_ARCH_KHAJE */
 #elif defined(__arch_um__)
 #define RMNET_TRACE_INCLUDE_PATH ../../datarmnet/core
 #else
-#define RMNET_TRACE_INCLUDE_PATH ../../../../../../../datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../sm6375-modules/qcom/opensource/datarmnet/core
 #endif /* defined(CONFIG_RMNET_LA_PLATFORM) */
 #endif /* RMNET_TRACE_INCLUDE_PATH */
 

qcom/opensource/datarmnet/core/rmnet_trace.h
@@ -9,11 +9,11 @@
 
 #ifndef RMNET_TRACE_INCLUDE_PATH
 #if defined(CONFIG_RMNET_LA_PLATFORM)
-#define RMNET_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../sm6375-modules/qcom/opensource/datarmnet/core
 #elif defined(__arch_um__)
 #define RMNET_TRACE_INCLUDE_PATH ../../datarmnet/core
 #else
-#define RMNET_TRACE_INCLUDE_PATH ../../../../../../../datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../sm6375-modules/qcom/opensource/datarmnet/core
 #endif /* defined(CONFIG_RMNET_LA_PLATFORM) */
 #endif /* RMNET_TRACE_INCLUDE_PATH */
 #define TRACE_INCLUDE_PATH RMNET_TRACE_INCLUDE_PATH

qcom/opensource/datarmnet/core/wda.h
@@ -10,14 +10,14 @@
 #ifndef RMNET_TRACE_INCLUDE_PATH
 #if defined(CONFIG_RMNET_LA_PLATFORM)
 #ifdef CONFIG_ARCH_KHAJE
-#define RMNET_TRACE_INCLUDE_PATH ../../../../../vendor/qcom/opensource/datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../../sm6375-modules/qcom/opensource/datarmnet/core
 #else
-#define RMNET_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../sm6375-modules/qcom/opensource/datarmnet/core
 #endif /* CONFIG_ARCH_KHAJE */
 #elif defined(__arch_um__)
 #define RMNET_TRACE_INCLUDE_PATH ../../datarmnet/core
 #else
-#define RMNET_TRACE_INCLUDE_PATH ../../../../../../../datarmnet/core
+#define RMNET_TRACE_INCLUDE_PATH ../../../../sm6375-modules/qcom/opensource/datarmnet/core
 #endif /* defined(CONFIG_RMNET_LA_PLATFORM) */
 #endif /* RMNET_TRACE_INCLUDE_PATH */
 #define TRACE_INCLUDE_PATH RMNET_TRACE_INCLUDE_PATH

qcom/opensource/dsp-kernel/dsp/fastrpc_trace.h
@@ -15,7 +15,7 @@
  * Define path if not defined in bazel file
  */
 #ifndef DSP_TRACE_INCLUDE_PATH
-#define DSP_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/dsp-kernel/dsp
+#define DSP_TRACE_INCLUDE_PATH ../../../../sm6375-modules/qcom/opensource/dsp-kernel/dsp
 #endif
 
 #undef TRACE_INCLUDE_PATH

qcom/opensource/securemsm-kernel/smcinvoke/compat/trace_smcinvoke.h
@@ -490,8 +490,7 @@ TRACE_EVENT(smcinvoke_release,
 * Define path if not defined in bazel file
  */
 #ifndef SMCINVOKE_TRACE_INCLUDE_PATH
-#define SMCINVOKE_TRACE_INCLUDE_PATH \
-	../../../../vendor/qcom/opensource/securemsm-kernel/smcinvoke/compat
+#define SMCINVOKE_TRACE_INCLUDE_PATH ../../../../sm6375-modules/qcom/opensource/securemsm-kernel/smcinvoke
 #endif
 
 #undef TRACE_INCLUDE_PATH